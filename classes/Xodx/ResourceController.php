ddd2b1a521fc10adbb9fbb79a16c352f8f516765
Norman Radtke <norman.radtke@gmail.com>
2012-08-14 13:31:57 +0200

    changed uri of activity object

--- classes/Xodx/ResourceController.php
+++ classes/Xodx/ResourceController.php
@@ -3,33 +3,31 @@
 require_once 'Tools.php';
 
 class Xodx_ResourceController extends Xodx_Controller
-{   
+(
     public function showAction($template)
     {
         $bootstrap = $this->_app->getBootstrap();
         $model = $bootstrap->getResource('model');
         $request = $bootstrap->getResource('request');
 
-        $objectUri = $request->getValue('objectUri', 'get');
+        $objectUri = $this->_app->getBaseUri() . '?c=resource&a=show&objectId=' . $request->getValue('objectId', 'get');
 
         $nsFoaf = 'http://xmlns.com/foaf/0.1/';
         $nsAair = 'http://xmlns.notu.be/aair#';
         $nsXodx = 'http://xodx.org/ns#';
 
         $objectQuery = 'PREFIX aair: <' . $nsAair. '> ' . 
-        	'PREFIX xodx: <' . $nsXodx. '> ' .
-            'SELECT ?type ?content ?image ?link ?mimeType' . 
-            'WHERE { ' .
+        'PREFIX xodx: <' . $nsXodx. '> ' .
+        'SELECT ?type ?content ?image ?link ?mimeType ' . 
+        'WHERE { ' .
             '   <' . $objectUri . '> a ?type . ' .
             '   OPTIONAL {<' . $objectUri . '> aair:largerImage ?image .} ' .
             '   OPTIONAL {<' . $objectUri . '> aair:type ?type .} ' .
             '   OPTIONAL {<' . $objectUri . '> aair:content ?content .} ' .
-        	'   OPTIONAL {<' . $objectUri . '> aair:targetURL ?link .} ' .
-        	'   OPTIONAL {<' . $objectUri . '> aair:mimeType ?mimeType .} ' .
+            '   OPTIONAL {<' . $objectUri . '> aair:targetURL ?link .} ' .
+            '   OPTIONAL {<' . $objectUri . '> aair:mimeType ?mimeType .} ' .
             '}';
-
         $object = $model->sparqlQuery($objectQuery);
-		var_dump($object);
         if (count($object) < 1) {
             $newStatements = Tools::getLinkedDataResource($objectUri);
             if ($newStatements !== null) {
@@ -45,25 +43,25 @@ class Xodx_ResourceController extends Xodx_Controller
                     'type' => $modelNew->getValue($objectUri, $nsAair . 'type'),
                     'content' => $modelNew->getValue($objectUri, $nsAair . 'content'),
                     'image' => $modelNew->getValue($objectUri, $nsAair . 'image'), 
-                	'mimeType' => $modelNew->getValue($objectUri, $nsAair . 'mimeType'),
-                	'link' => $modelNew->getValue($objectUri, $nsAair . 'link'),
+                    'mimeType' => $modelNew->getValue($objectUri, $nsAair . 'mimeType'),
+                    'link' => $modelNew->getValue($objectUri, $nsAair . 'link'),
                 );
             }
         }
-/**		// TODO getActivity with objectURI from Xodx_ActivityController
-        $personController = new Xodx_PersonController($this->_app);
+        // TODO getActivity with objectURI from Xodx_ActivityController
+        /**$personController = new Xodx_PersonController($this->_app);
         $activities = $personController->getActivities($personUri);
         $news = $personController->getNotifications($personUri);
-*/
+        */
         $template->resourceshowObjectUri = $objectUri;
         if (!empty($object[0]['image']))
-        	$template->resourceshowImage = $object[0]['image'];
+            $template->resourceshowImage = $object[0]['image'];
         if (!empty($object[0]['type']))
-        	$template->resourceshowType = $object[0]['type'];
-    	if (!empty($object[0]['content']))
-        	$template->resourceshowContent = $object[0]['content'];
+            $template->resourceshowType = $object[0]['type'];
         if (!empty($object[0]['content']))
-        	$template->resourceshowContent = $object[0]['content'];
+            $template->resourceshowContent = $object[0]['content'];
+        if (!empty($object[0]['content']))
+            $template->resourceshowContent = $object[0]['content'];
         $template->addContent('templates/resourceshow.phtml');
 
         return $template;
