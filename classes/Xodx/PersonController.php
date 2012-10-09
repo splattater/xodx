713462c72042a1606197273e63e68fc98442f129
Norman Radtke <norman.radtke@gmail.com>
2012-10-09 21:43:55 +0200

    move showAction of Profile to PersonController

--- classes/Xodx/PersonController.php
+++ classes/Xodx/PersonController.php
@@ -183,6 +183,79 @@ class Xodx_PersonController extends Xodx_ResourceController
         return $pingResult;
     }
 
+    public function showAction($template)
+    {
+        $bootstrap = $this->_app->getBootstrap();
+        $model = $bootstrap->getResource('model');
+        $request = $bootstrap->getResource('request');
+        $id = $request->getValue('id', 'get');
+        $controller = $request->getValue('c', 'get');
+
+        // get URI
+        $personUri = $this->_app->getBaseUri() . '?c=' . $controller . '&id=' . $id;
+
+        $nsFoaf = 'http://xmlns.com/foaf/0.1/';
+
+        $profileQuery = 'PREFIX foaf: <' . $nsFoaf . '> ' .
+            'SELECT ?depiction ?name ?nick ' .
+            'WHERE { ' .
+            '   <' . $personUri . '> a foaf:Person . ' .
+            '   OPTIONAL {<' . $personUri . '> foaf:depiction ?depiction .} ' .
+            '   OPTIONAL {<' . $personUri . '> foaf:name ?name .} ' .
+            '   OPTIONAL {<' . $personUri . '> foaf:nick ?nick .} ' .
+            '}';
+
+        // TODO deal with language tags
+        $contactsQuery = 'PREFIX foaf: <' . $nsFoaf . '> ' .
+            'SELECT ?contactUri ?name ' .
+            'WHERE { ' .
+            '   <' . $personUri . '> foaf:knows ?contactUri . ' .
+            '   OPTIONAL {?contactUri foaf:name ?name .} ' .
+            '}';
+
+        $profile = $model->sparqlQuery($profileQuery);
+
+        if (count($profile) < 1) {
+            $newStatements = Tools::getLinkedDataResource($personUri);
+            if ($newStatements !== null) {
+                $template->addDebug('Import Profile with LinkedDate');
+
+                $modelNew = new Erfurt_Rdf_MemoryModel($newStatements);
+                $newStatements = $modelNew->getStatements();
+
+                $template->addDebug(var_export($newStatements, true));
+
+                $profile = array();
+                $profile[0] = array(
+                    'depiction' => $modelNew->getValue($personUri, $nsFoaf . 'depiction'),
+                    'name' => $modelNew->getValue($personUri, $nsFoaf . 'name'),
+                    'nick' => $modelNew->getValue($personUri, $nsFoaf . 'nick')
+                );
+            }
+            //$knows = $modelNew->sparqlQuery($contactsQuery);
+
+            $knows = array();
+        } else {
+            $knows = $model->sparqlQuery($contactsQuery);
+        }
+
+        $activityController = $this->_app->getController('Xodx_ActivityController');
+        $personController = $this->_app->getController('Xodx_PersonController');
+        $activities = $activityController->getActivities($personUri);
+        $news = $personController->getNotifications($personUri);
+
+        $template->profileshowPersonUri = $personUri;
+        $template->profileshowDepiction = $profile[0]['depiction'];
+        $template->profileshowName = $profile[0]['name'];
+        $template->profileshowNick = $profile[0]['nick'];
+        $template->profileshowActivities = $activities;
+        $template->profileshowKnows = $knows;
+        $template->profileshowNews = $news;
+        $template->addContent('templates/profileshow.phtml');
+
+        return $template;
+    }
+
     /**
      * Quick fix for Erfurt issue #24 (https://github.com/AKSW/Erfurt/issues/24)
      */
