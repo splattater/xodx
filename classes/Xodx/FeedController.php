d5feed8d5dacef5cd6cd197e972c3c9062c37e8a
Norman Radtke <norman.radtke@gmail.com>
2012-10-18 11:16:19 +0200

    add test method

--- classes/Xodx/FeedController.php
+++ classes/Xodx/FeedController.php
@@ -92,4 +92,41 @@ class Xodx_FeedController extends Xodx_Controller
             $activityController->addActivities($activity);
         }
     }
+
+    public  function testFeedAction ()
+    {
+        $feed = '<?xml version="1.0" encoding="utf-8"?>
+<feed xmlns:activity="http://activitystrea.ms/schema/1.0/" xmlns="http://www.w3.org/2005/Atom">
+    <title>Activity Feed for Norman Radtke</title>
+    <id>http://splatatter.comiles.eu/xodx/?c=person&amp;id=splatte</id>
+    <link href="http://pubsubhubbub.appspot.com" rel="hub"/>
+    <link href="http://splatatter.comiles.eu/xodx/?c=feed&amp;a=getFeed&amp;uri=http%3A%2F%2Fsplatatter.comiles.eu%2Fxodx%2F%3Fc%3Dperson%26id%3Dsplatte" type="application/atom+xml" rel="self"/>
+    <updated>2012-10-17T13:46:58+02:00</updated>
+
+<entry>
+      <title>"Norman Radtke" did "http://xmlns.notu.be/aair#Post" a "http://rdfs.org/sioc/ns#Comment"</title>
+      <id>http://splatatter.comiles.eu/xodx/?c=resource&amp;id=77f46b1bb479c098c69997d914ab437f</id>
+      <link href="http://splatatter.comiles.eu/xodx/?c=resource&amp;id=77f46b1bb479c098c69997d914ab437f"/>
+      <published>2012-10-17T13:46:58+02:00</published>
+      <updated>2012-10-17T13:46:58+02:00</updated>
+      <author>
+        <name>Norman Radtke</name>
+        <uri>http://splatatter.comiles.eu/xodx/?c=person&amp;id=splatte</uri>
+      </author>
+      <activity:verb>http://xmlns.notu.be/aair#Post</activity:verb>
+      <activity:object>
+        <id>http://splatatter.comiles.eu/xodx/?c=resource&amp;id=4fb8e32ade09fe78ebaa97dd09cf94d5</id>
+
+        <content>Ein Test!</content>
+
+        <published>2012-10-17T13:46:58+02:00</published>
+
+        <activity:object-type>http://rdfs.org/sioc/ns#Comment</activity:object-type>
+              </activity:object>
+
+          </entry>
+</feed>
+        ';
+        $this->feedToActivity($feed);
+    }
 }
